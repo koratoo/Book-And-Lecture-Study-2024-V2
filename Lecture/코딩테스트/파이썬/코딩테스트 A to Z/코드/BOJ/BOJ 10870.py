759f3a782c775a20fb71ae1ab8f94bbdcfced28f
koratoo <ssyk2@naver.com>
2024-07-08 21:26:02 +0900

    [Docs] 파이썬

--- Lecture/코딩테스트/파이썬/코딩테스트 A to Z/코드/BOJ/BOJ 10870.py
+++ Lecture/코딩테스트/파이썬/코딩테스트 A to Z/코드/BOJ/BOJ 10870.py
@@ -1,5 +1,7 @@
 # 똑같은 값을 여러 번 구한다면, 배열에 저장하여 재사용하는 것이 좋다.
-
+# 재귀함수 + 중복계산 메모이제이션을 사용한다.
+# 몇 백만번 연산 -> 몇 십번 연산으로 바뀐다.
+# 값을 -1로 초기화 -> 값이 채워지지 않은 대상만 fib(x-1) + fib(x-2) 연산
 def fibo(x):
 	global arr
 
@@ -7,7 +9,7 @@ def fibo(x):
 		return arr[x]
 
 	arr[x] = fibo(x - 1) + fibo(x - 2)
-	return arr[x]
+	return arr[x] # 계산한 값을 저장한다는 개념 
 
 
 n = int(input())
